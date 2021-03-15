548e4ba52f9c555e34f01af74d36da1d4b926da3
lucian-student <56319974+lucian-student@users.noreply.github.com>
2021-03-15 17:08:46 +0100

    init

---
+++ server/configuration/db.js
@@ -0,0 +1,20 @@
+const Pool = require('pg').Pool;
+require('dotenv').config();
+/*
+lokalni konfigurace database
+pokud chcete zprovoznit vytvorte .env file
+v .env file promeny prostredi
+*/
+const pool = new Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB,
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+});
+
+
+module.exports = pool;
